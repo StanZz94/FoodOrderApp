8b6912291714a78743d5eda6e2090368a9c73e92
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-01-29 01:48:46 +0200

    create meals component and fetch meals

---
+++ src/components/Meals.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function Meals() {
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() => {
+        async function fetchMeals() {
+
+            const res = await fetch("http://localhost:3000/meals");
+    
+            if (!res.ok) {
+    
+            }
+    
+            const meals = await res.json();
+            setLoadedMeals(meals);
+        }
+
+        fetchMeals();
+    }, []);
+
+    return <ul id="meals">
+        {loadedMeals.map((meal) => (
+            <li key={meal.id}>{meal.name}</li>
+))}
+    </ul>
+}
