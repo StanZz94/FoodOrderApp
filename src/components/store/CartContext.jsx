9d9a99fa17ae7811819f3c23e5e6bf38c09f2ab3
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-03 00:11:37 +0200

    cart context remove item

--- src/components/store/CartContext.jsx
+++ src/components/store/CartContext.jsx
@@ -8,8 +8,7 @@ const CartContext = createContext({
 
 function cartReducer(state, action) {
     if (action.type === "ADD_ITEM") {
-
-        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id)
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
 
         const updatedItems = [...state.items];
 
@@ -24,11 +23,28 @@ function cartReducer(state, action) {
             updatedItems.push({ ...action.item, quantity: 1 });
         }
 
-        return { ...state, items: updatedItems }
+        return { ...state, items: updatedItems };
     }
 
     if (action.type === "REMOVE_ITEM") {
 
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedItems = [...state.items];
+
+        if (existingCartItem.quantity === 1) {
+            updatedItems.splice(existingCartItemIndex, 1);
+
+        } else {
+            const updatedItem = {
+                ...state.items,
+                quantity: existingCartItem.quantity - 1
+            };
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return { ...state, items: updatedItems };
     }
 
     return state;
