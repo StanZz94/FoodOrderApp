ca92384e0dba6aa62f6e360881c81a8fcc093c66
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-03 05:42:14 +0200

    add cartContext in Header and display cart quantity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
+import { useContext } from "react"
+
 import logo from "../assets/logo.jpg"
 import Button from "./UI/Button"
+import CartContext from "./store/CartContext"
 
 export default function Header() {
+
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity
+    }, 0)
+
     return <header id="main-header">
         <div id="title">
             <img src={logo} alt="resturant logo" />
             <h1>Food Order</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (2)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
